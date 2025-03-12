8a31939574907f42c0bb48419c3538d7bbc86692
Kiran Kumar Arsam <kiranarsam@users.noreply.github.com>
2025-03-12 17:15:41 +0530

    Updated with copyright holder

--- src/common/comm-if/src/can_comm_if.c
+++ src/common/comm-if/src/can_comm_if.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2025, Kiran Kumar Arsam
  * Copyright (c) 2024, COVESA
  * Copyright (c) 2019, Intel Corporation
  *
@@ -10,9 +11,10 @@
  *    * Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *    * Neither the name of Intel Corporation, COVESA nor the names of their
- *      contributors  may be used to endorse or promote products derived from
- *      this software without specific prior written permission.
+ *    * Neither the name of Intel Corporation, COVESA, Kiran Kumar Arsam nor
+ *      the names of their contributors  may be used to endorse or promote
+ *      products derived from this software without specific prior written
+ *      permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
