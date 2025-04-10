1a4cc2fab6809bb5138ab941ffa49080a2535c48
Kiran Kumar Arsam <kiranarsam@users.noreply.github.com>
2025-04-10 17:44:39 +0530

    Sample frontend ui file modification

--- src/apps/gui-app/vehicle-app/src/App.js
+++ src/apps/gui-app/vehicle-app/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+
+import Add from "./components/Add";
+import Search from "./components/Search";
+import List from "./components/List";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/tutorials"} className="navbar-brand m-1">
+            Home
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Lists
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-2">
+          <Routes>
+            <Route path="/" element={<List/>} />
+            <Route path="/tutorials" element={<List/>} />
+            <Route path="/add" element={<Add/>} />
+            <Route path="/tutorials/:id" element={<Search/>} />
+          </Routes>
+        </div>
+      </div>
   );
 }
 
